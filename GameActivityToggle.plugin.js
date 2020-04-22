bdfe97525285801188822073828a55f5af3cd338
egrodo <egrodo1@gmail.com>
2020-04-22 13:23:15 -0400

    Fixing bug that made it not work for non-English speakers by changing selector criteria

--- GameActivityToggle.plugin.js
+++ GameActivityToggle.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name GameActivityToggle
- * @version 1.2.1
+ * @version 1.2.2
  * @description Simple plugin that adds the \"display game activity\" setting
  * on the home toolbar so you can toggle it easier when you don't want your friends knowing how much you play video games.
  *
@@ -40,6 +40,8 @@ const disabledIcon =
 const disabledIconHover =
   '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 24 24" width="20" height="20" xml:space="preserve"><style type="text/css">.st0{fill:#dcddde;}.st1{fill:#F04747;}</style><g><path class="st0" d="M17.7,5.7h-0.8L4.4,18.1c1-0.2,1.9-0.8,2.3-1.8l0.1-0.3c0.3-0.6,0.9-1,1.6-1h1.9l4.7-4.6v0c-0.1-0.1-0.1-0.2-0.1-0.4c0-0.4,0.3-0.8,0.8-0.8c0,0,0,0,0,0c0.1,0,0.2,0,0.3,0.1l0.5-0.5c-0.1-0.1-0.1-0.2-0.1-0.4c0-0.4,0.3-0.8,0.8-0.8c0.1,0,0.3,0,0.4,0.1l1.7-1.7C18.8,5.8,18.3,5.7,17.7,5.7z M23.5,13.4l-2.8-5.8c0,0,0-0.1-0.1-0.1l-1.8,1.8c0.4,0,0.7,0.4,0.7,0.8c0,0.4-0.3,0.8-0.8,0.8c-0.4,0-0.8-0.3-0.8-0.7l-0.8,0.8c0.4,0,0.7,0.4,0.7,0.8c0,0.4-0.4,0.8-0.8,0.8c-0.4,0-0.8-0.3-0.8-0.7L13.1,15h2.4c0.7,0,1.3,0.4,1.6,1l0.1,0.3c0.8,1.6,2.7,2.3,4.4,1.6l0.3-0.1C23.6,17,24.3,15,23.5,13.4z M6.3,5.7C5,5.7,3.8,6.4,3.3,7.6l-2.8,5.8c0,0,0,0,0,0C-0.3,15,0.4,16.9,2,17.7L14,5.7H6.3z M8.2,10.8H7.3c-0.2,0-0.4,0.2-0.4,0.3v0.9c0,0.2-0.2,0.3-0.3,0.3H5.7c-0.2,0-0.3-0.2-0.3-0.3v-0.9c0-0.2-0.2-0.3-0.4-0.3H4.1c-0.2,0-0.4-0.2-0.4-0.4V9.6c0-0.2,0.2-0.4,0.4-0.4H5c0.2,0,0.4-0.2,0.4-0.4V8c0-0.2,0.2-0.4,0.4-0.4h0.8C6.8,7.7,7,7.8,7,8v0.9c0,0.2,0.2,0.4,0.4,0.4h0.9c0.2,0,0.3,0.2,0.3,0.4v0.8C8.6,10.7,8.4,10.8,8.2,10.8z"/><polygon points="19.3,11.1 19.3,11.1 19.3,11.1 "/><polygon points="19.3,11.2 19.3,11.1 19.3,11.1 "/></g><polygon class="st1" points="22.6,2.7 22.6,2.8 19.3,6.1 16,9.3 16,9.4 15,10.4 15,10.4 10.3,15 2.8,22.5 1.4,21.1 21.2,1.3 "/></svg>';
 
+const muteIconPath = `M14.99 11C14.99 12.66 13.66 14 12 14C10.34 14 9 12.66 9 11V5C9 3.34 10.34 2 12 2C13.66 2 15 3.34 15 5L14.99 11ZM12 16.1C14.76 16.1 17.3 14 17.3 11H19C19 14.42 16.28 17.24 13 17.72V21H11V17.72C7.72 17.23 5 14.41 5 11H6.7C6.7 14 9.24 16.1 12 16.1ZM12 4C11.2 4 11 4.66667 11 5V11C11 11.3333 11.2 12 12 12C12.8 12 13 11.3333 13 11V5C13 4.66667 12.8 4 12 4Z`;
+
 class GameActivityToggle {
   btnReference = null;
   tooltipReference = null;
@@ -64,7 +66,7 @@ class GameActivityToggle {
   }
 
   getVersion() {
-    return '1.2.1';
+    return '1.2.2';
   }
 
   getAuthor() {
@@ -106,17 +108,29 @@ class GameActivityToggle {
     // If there are multiple elements found with this selector then the user is most likely in a call. Use the appropriate one
     const rows = document.querySelectorAll(`.${selector}`);
     let row;
-    console.log(rows);
     if (rows.length) {
-      // Find the correct row
+      // Find the correct row by looking for one who's DOM structure matches what we expect
       for (let i = 0; i < rows.length; ++i) {
-        if (rows[i].firstElementChild && rows[i].firstElementChild.getAttribute('aria-label') === 'Mute') {
-          row = rows[i];
-          break;
+        try {
+          if (
+            rows[i].firstElementChild.firstElementChild.firstElementChild.firstElementChild.getAttribute('d') ===
+            muteIconPath
+          ) {
+            row = rows[i];
+            break;
+          }
+        } catch (err) {
+          // If the above firstELementChild accessing fails assume it's not the correct row and continue
+          continue;
         }
       }
     } else {
       row = rows[0];
+    }
+
+    if (!row) {
+      console.log(rows);
+      throw new Error('Could not find correct row?');
     }
 
     this.btnReference = row.firstElementChild.cloneNode(true);
