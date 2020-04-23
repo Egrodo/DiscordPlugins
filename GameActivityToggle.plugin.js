c8fd3ed880ebc30b06fc66dff7f87cb42e803008
Noah Yamamoto <egrodo1@gmail.com>
2020-04-23 17:06:26 -0600

    Addiing both icon states to conditional

--- GameActivityToggle.plugin.js
+++ GameActivityToggle.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name GameActivityToggle
- * @version 1.2.2
+ * @version 1.2.3
  * @description Simple plugin that adds the \"display game activity\" setting
  * on the home toolbar so you can toggle it easier when you don't want your friends knowing how much you play video games.
  *
@@ -40,6 +40,7 @@ const disabledIcon =
 const disabledIconHover =
   '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 24 24" width="20" height="20" xml:space="preserve"><style type="text/css">.st0{fill:#dcddde;}.st1{fill:#F04747;}</style><g><path class="st0" d="M17.7,5.7h-0.8L4.4,18.1c1-0.2,1.9-0.8,2.3-1.8l0.1-0.3c0.3-0.6,0.9-1,1.6-1h1.9l4.7-4.6v0c-0.1-0.1-0.1-0.2-0.1-0.4c0-0.4,0.3-0.8,0.8-0.8c0,0,0,0,0,0c0.1,0,0.2,0,0.3,0.1l0.5-0.5c-0.1-0.1-0.1-0.2-0.1-0.4c0-0.4,0.3-0.8,0.8-0.8c0.1,0,0.3,0,0.4,0.1l1.7-1.7C18.8,5.8,18.3,5.7,17.7,5.7z M23.5,13.4l-2.8-5.8c0,0,0-0.1-0.1-0.1l-1.8,1.8c0.4,0,0.7,0.4,0.7,0.8c0,0.4-0.3,0.8-0.8,0.8c-0.4,0-0.8-0.3-0.8-0.7l-0.8,0.8c0.4,0,0.7,0.4,0.7,0.8c0,0.4-0.4,0.8-0.8,0.8c-0.4,0-0.8-0.3-0.8-0.7L13.1,15h2.4c0.7,0,1.3,0.4,1.6,1l0.1,0.3c0.8,1.6,2.7,2.3,4.4,1.6l0.3-0.1C23.6,17,24.3,15,23.5,13.4z M6.3,5.7C5,5.7,3.8,6.4,3.3,7.6l-2.8,5.8c0,0,0,0,0,0C-0.3,15,0.4,16.9,2,17.7L14,5.7H6.3z M8.2,10.8H7.3c-0.2,0-0.4,0.2-0.4,0.3v0.9c0,0.2-0.2,0.3-0.3,0.3H5.7c-0.2,0-0.3-0.2-0.3-0.3v-0.9c0-0.2-0.2-0.3-0.4-0.3H4.1c-0.2,0-0.4-0.2-0.4-0.4V9.6c0-0.2,0.2-0.4,0.4-0.4H5c0.2,0,0.4-0.2,0.4-0.4V8c0-0.2,0.2-0.4,0.4-0.4h0.8C6.8,7.7,7,7.8,7,8v0.9c0,0.2,0.2,0.4,0.4,0.4h0.9c0.2,0,0.3,0.2,0.3,0.4v0.8C8.6,10.7,8.4,10.8,8.2,10.8z"/><polygon points="19.3,11.1 19.3,11.1 19.3,11.1 "/><polygon points="19.3,11.2 19.3,11.1 19.3,11.1 "/></g><polygon class="st1" points="22.6,2.7 22.6,2.8 19.3,6.1 16,9.3 16,9.4 15,10.4 15,10.4 10.3,15 2.8,22.5 1.4,21.1 21.2,1.3 "/></svg>';
 
+const micIconPath = `M6.7 11H5C5 12.19 5.34 13.3 5.9 14.28L7.13 13.05C6.86 12.43 6.7 11.74 6.7 11Z`;
 const muteIconPath = `M14.99 11C14.99 12.66 13.66 14 12 14C10.34 14 9 12.66 9 11V5C9 3.34 10.34 2 12 2C13.66 2 15 3.34 15 5L14.99 11ZM12 16.1C14.76 16.1 17.3 14 17.3 11H19C19 14.42 16.28 17.24 13 17.72V21H11V17.72C7.72 17.23 5 14.41 5 11H6.7C6.7 14 9.24 16.1 12 16.1ZM12 4C11.2 4 11 4.66667 11 5V11C11 11.3333 11.2 12 12 12C12.8 12 13 11.3333 13 11V5C13 4.66667 12.8 4 12 4Z`;
 
 class GameActivityToggle {
@@ -58,7 +59,7 @@ class GameActivityToggle {
   }
 
   getName() {
-    return 'Game Activity Toggle';
+    return "Game Activity Toggle";
   }
 
   getDescription() {
@@ -66,11 +67,11 @@ class GameActivityToggle {
   }
 
   getVersion() {
-    return '1.2.2';
+    return "1.2.3";
   }
 
   getAuthor() {
-    return 'egrodo';
+    return "egrodo";
   }
 
   load() {
@@ -79,29 +80,29 @@ class GameActivityToggle {
       ZLibrary.PluginUpdater.checkForUpdate(
         this.getName(),
         this.getVersion(),
-        'https://raw.githubusercontent.com/Egrodo/DiscordPlugins/master/GameActivityToggle.plugin.js',
+        "https://raw.githubusercontent.com/Egrodo/DiscordPlugins/master/GameActivityToggle.plugin.js"
       );
     }
   }
 
   start() {
     // On start check what game activity is currently set to.
-    this.gameActivity = BdApi.findModuleByProps('guildPositions').showCurrentGame;
-    this.soundsReference = BdApi.findModuleByProps('playSound');
+    this.gameActivity = BdApi.findModuleByProps("guildPositions").showCurrentGame;
+    this.soundsReference = BdApi.findModuleByProps("playSound");
 
     // Create our DOM elements
     this.createButton();
     this.createTooltip();
 
     // Watch for change
-    BdApi.findModuleByProps('guildPositions').addChangeListener(this.checkForChange);
+    BdApi.findModuleByProps("guildPositions").addChangeListener(this.checkForChange);
   }
 
   createButton() {
     // Use flexMarginReset prop to find the selector for the taskbar row.
-    const selector = (BdApi.findModuleByProps('flexMarginReset', 'flex').flex || '').split(' ')[0];
+    const selector = (BdApi.findModuleByProps("flexMarginReset", "flex").flex || "").split(" ")[0];
     if (!selector) {
-      console.error('GameActivityToggle failed to start up: Selector not found?');
+      console.error("GameActivityToggle failed to start up: Selector not found?");
       return;
     }
 
@@ -113,8 +114,10 @@ class GameActivityToggle {
       for (let i = 0; i < rows.length; ++i) {
         try {
           if (
-            rows[i].firstElementChild.firstElementChild.firstElementChild.firstElementChild.getAttribute('d') ===
-            muteIconPath
+            rows[i].firstElementChild.firstElementChild.firstElementChild.firstElementChild.getAttribute("d") ===
+              muteIconPath ||
+            rows[i].firstElementChild.firstElementChild.firstElementChild.firstElementChild.getAttribute("d") ===
+              micIconPath
           ) {
             row = rows[i];
             break;
@@ -130,19 +133,19 @@ class GameActivityToggle {
 
     if (!row) {
       console.log(rows);
-      throw new Error('Could not find correct row?');
+      throw new Error("Could not find correct row?");
     }
 
     this.btnReference = row.firstElementChild.cloneNode(true);
     this.btnReference.firstElementChild.innerHTML = this.gameActivity ? enabledIcon : disabledIcon;
-    this.btnReference.firstElementChild.style.pointerEvents = 'none'; // Ignore pointer events to fix bug that was causing repeated clicks to be ignored.
-    this.btnReference.id = 'GameActivityToggleBtn';
-    this.btnReference.setAttribute('aria-label', 'Toggle Game Activity');
-    this.btnReference.setAttribute('aria-checked', `${this.gameActivity ? 'true' : 'false'}`);
+    this.btnReference.firstElementChild.style.pointerEvents = "none"; // Ignore pointer events to fix bug that was causing repeated clicks to be ignored.
+    this.btnReference.id = "GameActivityToggleBtn";
+    this.btnReference.setAttribute("aria-label", "Toggle Game Activity");
+    this.btnReference.setAttribute("aria-checked", `${this.gameActivity ? "true" : "false"}`);
 
-    this.btnReference.addEventListener('click', this.onToggle);
-    this.btnReference.addEventListener('mouseenter', this.onButtonMouseOver);
-    this.btnReference.addEventListener('mouseleave', this.onButtonMouseOut);
+    this.btnReference.addEventListener("click", this.onToggle);
+    this.btnReference.addEventListener("mouseenter", this.onButtonMouseOver);
+    this.btnReference.addEventListener("mouseleave", this.onButtonMouseOut);
     row.prepend(this.btnReference);
 
     // Observe changes on the row to watch for our element being overwritten.
@@ -154,20 +157,20 @@ class GameActivityToggle {
 
   createTooltip() {
     // Also setup my recreated tooltip that uses Discord's classes.
-    const tooltipClasses = BdApi.findModuleByProps('tooltipBottom');
+    const tooltipClasses = BdApi.findModuleByProps("tooltipBottom");
 
-    const wrapperDiv = document.createElement('div');
-    wrapperDiv.style.visibility = 'hidden';
-    wrapperDiv.style.position = 'absolute';
-    wrapperDiv.style.zIndex = '10000';
-    wrapperDiv.style.pointerEvents = 'none';
+    const wrapperDiv = document.createElement("div");
+    wrapperDiv.style.visibility = "hidden";
+    wrapperDiv.style.position = "absolute";
+    wrapperDiv.style.zIndex = "10000";
+    wrapperDiv.style.pointerEvents = "none";
     this.tooltipReference = wrapperDiv;
 
-    const textWrapper = document.createElement('div');
-    textWrapper.className = [tooltipClasses.tooltip, tooltipClasses.tooltipTop, tooltipClasses.tooltipBlack].join(' ');
-    textWrapper.innerText = `Turn ${this.gameActivity ? 'off' : 'on'} game activity`;
+    const textWrapper = document.createElement("div");
+    textWrapper.className = [tooltipClasses.tooltip, tooltipClasses.tooltipTop, tooltipClasses.tooltipBlack].join(" ");
+    textWrapper.innerText = `Turn ${this.gameActivity ? "off" : "on"} game activity`;
 
-    const bottomArrow = document.createElement('div');
+    const bottomArrow = document.createElement("div");
     bottomArrow.className = tooltipClasses.tooltipPointer;
 
     textWrapper.prepend(bottomArrow);
@@ -177,22 +180,22 @@ class GameActivityToggle {
 
   onToggle() {
     this.gameActivity = !this.gameActivity;
-    BdApi.findModuleByProps('updateRemoteSettings').updateLocalSettings({ showCurrentGame: this.gameActivity });
-    BdApi.findModuleByProps('updateRemoteSettings').updateRemoteSettings({ showCurrentGame: this.gameActivity });
+    BdApi.findModuleByProps("updateRemoteSettings").updateLocalSettings({ showCurrentGame: this.gameActivity });
+    BdApi.findModuleByProps("updateRemoteSettings").updateRemoteSettings({ showCurrentGame: this.gameActivity });
     this.btnReference.firstElementChild.innerHTML = this.gameActivity ? enabledIcon : disabledIcon;
 
     // In order to preserve the tooltipPointer but also change the message we have to do this
-    const innerTooltipHTML = this.tooltipReference.firstElementChild.innerHTML.split('Turn');
+    const innerTooltipHTML = this.tooltipReference.firstElementChild.innerHTML.split("Turn");
     this.tooltipReference.firstElementChild.innerHTML = `${innerTooltipHTML[0]} Turn ${
-      this.gameActivity ? 'off' : 'on'
+      this.gameActivity ? "off" : "on"
     } game activity`;
 
-    this.btnReference.setAttribute('aria-checked', `${this.gameActivity ? 'true' : 'false'}`);
+    this.btnReference.setAttribute("aria-checked", `${this.gameActivity ? "true" : "false"}`);
 
     // Play the mute / unmute sound on toggle.
     if (this.gameActivity) {
-      this.soundsReference.playSound('unmute', 0.4);
-    } else this.soundsReference.playSound('mute', 0.4);
+      this.soundsReference.playSound("unmute", 0.4);
+    } else this.soundsReference.playSound("mute", 0.4);
   }
 
   // On mouse over swap icons to highlight and display tooltip in correct position.
@@ -204,45 +207,45 @@ class GameActivityToggle {
     const tooltipYPos = y - target.clientHeight - 8; // 8 being a constant amount of space to hover above the btn.
 
     this.tooltipReference.style.left = `${tooltipXPos}px`;
-    this.tooltipReference.style.visibility = 'visible';
+    this.tooltipReference.style.visibility = "visible";
     this.tooltipReference.style.top = `${tooltipYPos}px`;
 
-    this.tooltipReference.visibility = 'visible';
+    this.tooltipReference.visibility = "visible";
   }
 
   onButtonMouseOut() {
     this.btnReference.firstElementChild.innerHTML = this.gameActivity ? enabledIcon : disabledIcon;
-    this.tooltipReference.style.visibility = 'hidden';
+    this.tooltipReference.style.visibility = "hidden";
   }
 
   // Certain UI actions can result in the row being re-rendered and the button removed. Watch the row and re-add the button when necessary.
   checkForRemoval() {
-    if (!document.getElementById('GameActivityToggleBtn')) {
+    if (!document.getElementById("GameActivityToggleBtn")) {
       this.createButton();
     }
   }
 
   // We need to check for the user toggling game activity in the actual settings menu as well, because we have no event for that.
   checkForChange() {
-    if (this.gameActivity !== BdApi.findModuleByProps('guildPositions').showCurrentGame) {
-      this.gameActivity = BdApi.findModuleByProps('guildPositions').showCurrentGame;
+    if (this.gameActivity !== BdApi.findModuleByProps("guildPositions").showCurrentGame) {
+      this.gameActivity = BdApi.findModuleByProps("guildPositions").showCurrentGame;
 
       this.btnReference.firstElementChild.innerHTML = this.gameActivity ? enabledIcon : disabledIcon;
 
-      const innerTooltipHTML = this.tooltipReference.firstElementChild.innerHTML.split('Turn');
+      const innerTooltipHTML = this.tooltipReference.firstElementChild.innerHTML.split("Turn");
       this.tooltipReference.firstElementChild.innerHTML = `${innerTooltipHTML[0]} Turn ${
-        this.gameActivity ? 'off' : 'on'
+        this.gameActivity ? "off" : "on"
       } game activity`;
 
-      this.btnReference.setAttribute('aria-checked', `${this.gameActivity ? 'true' : 'false'}`);
-      this.btnReference.addEventListener('click', this.onToggle);
+      this.btnReference.setAttribute("aria-checked", `${this.gameActivity ? "true" : "false"}`);
+      this.btnReference.addEventListener("click", this.onToggle);
     }
   }
 
   stop() {
     this.observer.disconnect();
-    this.btnReference.removeEventListener('mouseenter', this.onButtonMouseOver);
-    this.btnReference.removeEventListener('mouseleave', this.onButtonMouseOut);
+    this.btnReference.removeEventListener("mouseenter", this.onButtonMouseOver);
+    this.btnReference.removeEventListener("mouseleave", this.onButtonMouseOut);
     this.btnReference.parentNode.removeChild(this.btnReference);
     this.tooltipReference.parentNode.removeChild(this.tooltipReference);
   }
